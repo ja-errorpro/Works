c204adc04830e5f60990524aaa798a1ddddd5fe9
Nick810026 <u810026@shsh.tw>
2020-10-23 22:30:18 +0800

    2020/10/23

---
+++ Tetris/Assets/Drop_blue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drop_blue : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += new Vector3(0,-2.5f,0) * Time.deltaTime;
+    }
+}
