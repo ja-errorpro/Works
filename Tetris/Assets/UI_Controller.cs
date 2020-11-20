f1a5cb90f5a0b68c1944d40b99fe6166cb0efd1a
Nick810026 <u810026@shsh.tw>
2020-11-20 14:10:04 +0800

    11/20

---
+++ Tetris/Assets/UI_Controller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    public Sprite[] SpriteTexture = new Sprite[2];
+    private SpriteRenderer Sprite_Renderer;
+    void Start()
+    {
+        Sprite_Renderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMobileEnter(){
+        Sprite_Renderer.sprite = SpriteTexture[1];
+    }
+    void OnMobileExit(){
+        Sprite_Renderer.sprite = SpriteTexture[0];
+    }
+}
