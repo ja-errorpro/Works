9b06c93f29fa96637cf6015c1473703c4dba1c4f
Nick810026 <u810026@shsh.tw>
2020-12-25 20:57:04 +0800

    1225

---
+++ Tetris/Assets/TetrisSport.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetrisSport : MonoBehaviour
+{
+    private float previousTime;
+    public float falltime = 0.8f;
+    public static int height = 12;
+    public static int width = 6;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3 (-0.3f,0,0);
+        }
+        else if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3 (0.3f,0,0);
+        }
+
+        if(Time.time - previousTime > falltime)
+        {
+            transform.position += new Vector3 (0,-0.3f,0);
+            previousTime = Time.time;
+        }
+    }
+
+    /*bool ValidMove()
+    {
+        foreach(Transform children in transform)
+        {
+            int roundedX = Mathf.RoundToInt(children.transform.position.x);
+            int roundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundedX < 0 || roundedX >=width || roundedY < 0 || roundedY >= height)
+            {
+                return false;
+            }
+        }
+        return true;
+    }*/
+}
--- Tetris/Assets/UI_Controller.cs
+++ Tetris/Assets/UI_Controller.cs
@@ -7,58 +7,17 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class UI_Controller : MonoBehaviour
 {
-    public Sprite[] SpriteTexture = new Sprite[2];
-    private SpriteRenderer Sprite_Renderer;
-    Vector2 m_screenPos = new Vector2();
     void Start()
     {
-        Sprite_Renderer = this.GetComponent<SpriteRenderer>();
+        
     }
-
     // Update is called once per frame
     void Update()
     {
-      /*  if (Input.GetKeyDown(KeyCode.W))
-    {
-        Sprite_Renderer.sprite = SpriteTexture[1];
+      
     }
-    if (Input.GetKeyUp(KeyCode.W))
+    public void click()
     {
-        Sprite_Renderer.sprite = SpriteTexture[0];
-        Thread.Sleep(150); 
-        SceneManager.LoadScene("Scenes/SampleScene");
-    }*/
-    /*if (Input.touches[0] == TouchPhase.Began ){
-        Sprite_Renderer.sprite = SpriteTexture[1];
-    }
-    if (Input.touches[0] == TouchPhase.Ended ){
-        Sprite_Renderer.sprite = SpriteTexture[0];
-        Thread.Sleep(150); 
-        SceneManager.LoadScene("Scenes/SampleScene");
-    }*/
-    /*if(Input.touchCount == 1){
-        Touch touch = Input.GetTouch(0);
-        bool isTouchSTART = EventSystem.current.IsPointerOverGameObject(touch.fingerId);
-        if(isTouchSTART){
-            switch(touch.phase){
-                case TouchPhase.Began:
-                    Sprite_Renderer.sprite = SpriteTexture[1];
-                    break;
-                case TouchPhase.Ended:
-                    Sprite_Renderer.sprite = SpriteTexture[0];
-                    Thread.Sleep(150); 
-                    SceneManager.LoadScene("Scenes/SampleScene");
-                    break;
-            }
-        }
-    }*/
-    }
-    public void startclick()
-    {
-        Sprite_Renderer.sprite = SpriteTexture[1]; 
-        Thread.Sleep(200);
-        Sprite_Renderer.sprite = SpriteTexture[0];
-        Thread.Sleep(150); 
         SceneManager.LoadScene("Scenes/SampleScene");
     }
 }
