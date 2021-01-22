32dc5aa78059bee9e05d74e8cb6959a6f5039a94
Nick810026 <u810026@shsh.tw>
2021-01-22 21:36:16 +0800

    2021/1/22

--- Tetris/Assets/TetrisSport.cs
+++ Tetris/Assets/TetrisSport.cs
@@ -5,11 +5,11 @@ using System.Threading;
 
 public class TetrisSport : MonoBehaviour
 {    
-    public Vector3 rotationPoint;
     private float previousTime;
     public float falltime = 0.5f;
     public static int height = 12;
-    public static int width = 6;
+    public static float width = 5.4f;
+    public GameObject rig;
     void Start()
     {
         
@@ -20,41 +20,40 @@ public class TetrisSport : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3 (-0.6f,0,0);
-            if(!ValidMove())
-                transform.position -= new Vector3 (-0.6f,0,0);
+            gameObject.transform.position += new Vector3 (-0.6f,0,0);
+            if(ValidMove() == false) 
+                gameObject.transform.position -= new Vector3 (-0.6f,0,0);
         }
         else if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            transform.position += new Vector3 (0.6f,0,0);
-            if(!ValidMove())
-                transform.position -= new Vector3 (0.6f,0,0);
+            gameObject.transform.position += new Vector3 (0.6f,0,0);
+            if(ValidMove() == false)
+                gameObject.transform.position -= new Vector3 (0.6f,0,0);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             //rotate !
-            transform.RotateAround(transform.TransformPoint(rotationPoint),new Vector3(0,0,1), 90);
-            if (!ValidMove())
-                transform.RotateAround(transform.TransformPoint(rotationPoint),new Vector3(0,0,1), -90);
+            rig.transform.eulerAngles += new Vector3(0,0,90);
+            if(ValidMove() == false)
+                rig.transform.eulerAngles -= new Vector3(0,0,90);
         }
 
         if(Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? falltime / 10 :falltime))
         {
-            transform.position += new Vector3 (0,-0.6f,0);
-            if(!ValidMove())
-                transform.position -= new Vector3 (0,-0.6f,0);
+            gameObject.transform.position += new Vector3 (0,-0.6f,0);
+            if(ValidMove() == false)
+                gameObject.transform.position -= new Vector3 (0,-0.6f,0);
             previousTime = Time.time;
         }
     }
 
     bool ValidMove()
     {
-        foreach(Transform children in transform)
+        foreach(Transform subBlock in rig.transform)
         {
-            int roundedX = Mathf.RoundToInt(children.transform.position.x);
-            int roundedY = Mathf.RoundToInt(children.transform.position.y);
-
-            if(roundedX < 0 || roundedX >=width || roundedY < 0 || roundedY >= height)
+            if( subBlock.transform.position.x < 0 ||
+                subBlock.transform.position.x >= width ||
+                subBlock.transform.position.y < 0 )
             {
                 return false;
             }
