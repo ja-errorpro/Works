1bb4eb59555c6f6e18b5d21c4c01bc4e6add1c21
Nick810026 <u810026@shsh.tw>
2021-01-27 22:42:51 +0800

    1/27

---
+++ Tetris/Assets/GameLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static float quickfalltime = 0.05f;
+    public static float falltime = 0.5f;
+    public static int height = 12;
+    public static int width = 6;
+    public GameObject[] blocks;
+    //public Transform[,] grid = new Transform[width,height];
+    void Start()
+    {
+        SpawnBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SpawnBlock()
+    {
+        Instantiate(blocks[Random.Range(0,blocks.Length)], transform.position, Quaternion.identity);
+    }
+}
--- Tetris/Assets/TetrisSport.cs
+++ Tetris/Assets/TetrisSport.cs
@@ -7,14 +7,12 @@ public class TetrisSport : MonoBehaviour
 {    
     bool movable = true;
     public static float time = 0;
-    public static float quickfalltime = 0.05f;
-    public float falltime = 0.5f;
-    public static float height = 12f;
-    public static float width = 5.4f;
     public GameObject rig;
+    GameLogic gameLogic; 
+    public Vector3 rotationPoint;
     void Start()
     {
-        
+        gameLogic = FindObjectOfType<GameLogic>();
     }
 
     // Update is called once per frame
@@ -23,50 +21,66 @@ public class TetrisSport : MonoBehaviour
         if(movable)
         {
             time += 1 * Time.deltaTime;
-            if(time > quickfalltime && Input.GetKey(KeyCode.DownArrow))
+            if(time > GameLogic.quickfalltime && Input.GetKey(KeyCode.DownArrow))
             {
                 gameObject.transform.position += new Vector3 (0,-0.6f,0);
                 time = 0;
-                if(ValidMove() == false) 
+                if(!ValidMove()) 
+                {
+                    movable = false;
                     gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+                   // RegisterBlock();
+                    gameLogic.SpawnBlock();
+                }    
                 //Debug.Log(gameObject.transform.position);
             }
-            else if(time > falltime)
+            else if(time > GameLogic.falltime)
             {
-                rig.transform.position += new Vector3 (0,-0.6f,0);
+                gameObject.transform.position += new Vector3 (0,-0.6f,0);
                 time = 0;
-                if(ValidMove() == false) 
-                    rig.transform.position -= new Vector3 (0,-0.6f,0);
+                if(!ValidMove()) 
+                {
+                    movable = false;
+                    gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+                    //RegisterBlock();
+                    gameLogic.SpawnBlock();
+                }    
                 //Debug.Log(gameObject.transform.position);
             }
             if(Input.GetKeyDown(KeyCode.LeftArrow))
             {
                 gameObject.transform.position += new Vector3 (-0.6f,0,0);
-                if(ValidMove() == false) 
+                if(!ValidMove()) 
                     gameObject.transform.position -= new Vector3 (-0.6f,0,0);
             }
             else if(Input.GetKeyDown(KeyCode.RightArrow))
             {
                 gameObject.transform.position += new Vector3 (0.6f,0,0);
-                if(ValidMove() == false)
+                if(!ValidMove())
                     gameObject.transform.position -= new Vector3 (0.6f,0,0);
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 //rotate !
-                rig.transform.eulerAngles += new Vector3(0,0,90);
-                if(ValidMove() == false)
-                    rig.transform.eulerAngles -= new Vector3(0,0,90);
+                rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
+                if(!ValidMove())
+                    rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
             }
         }
     }
-
+    /*void RegisterBlock()
+    {
+        foreach(Transform subBlock in rig.transform)
+        {
+            gameLogic.grid[Mathf.RoundToInt(subBlock.position.x), Mathf.RoundToInt(subBlock.position.y)] = subBlock;
+        }
+    }*/
     bool ValidMove()
     {
         foreach(Transform subBlock in rig.transform)
         {
             if( subBlock.transform.position.x < 0 ||
-                subBlock.transform.position.x >= width ||
+                subBlock.transform.position.x >= GameLogic.width ||
                 subBlock.transform.position.y < 0 )
             {
                 print(subBlock.name);
@@ -74,7 +88,48 @@ public class TetrisSport : MonoBehaviour
                 Debug.Log(subBlock.transform.position.y);
                 return false;
             }
+            /*if(gameLogic.grid[Mathf.RoundToInt(subBlock.position.x), Mathf.RoundToInt(subBlock.position.y)] != null)
+            {
+                return false;
+            }*/
         }
         return true;
     }
+    /*void leftclick()
+    {
+        gameObject.transform.position += new Vector3 (-0.6f,0,0);
+        if(ValidMove() == false) 
+            gameObject.transform.position -= new Vector3 (-0.6f,0,0);
+    }
+    void rightclick()
+    {
+        gameObject.transform.position += new Vector3 (0.6f,0,0);
+        if(ValidMove() == false)
+            gameObject.transform.position -= new Vector3 (0.6f,0,0);
+    }
+    void turnclick()
+    {
+        rig.transform.eulerAngles += new Vector3(0,0,90);
+        if(ValidMove() == false)
+            rig.transform.eulerAngles -= new Vector3(0,0,90);
+    }
+    void downclick()
+    {
+        if(time > quickfalltime && Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3 (0,-0.6f,0);
+            time = 0;
+            if(ValidMove() == false) 
+                gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+            //Debug.Log(gameObject.transform.position);
+        }
+        else if(time > falltime)
+        {
+            gameObject.transform.position += new Vector3 (0,-0.6f,0);
+            time = 0;
+            if(ValidMove() == false) 
+                gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+            //Debug.Log(gameObject.transform.position);
+        }
+    }*/
 }
