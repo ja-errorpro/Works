b486b04167d4443bf4bdb5d9c6dd350704690348
Nick810026 <u810026@shsh.tw>
2021-01-30 22:29:31 +0800

    1/30

--- Tetris/Assets/GameLogic.cs
+++ Tetris/Assets/GameLogic.cs
@@ -5,14 +5,7 @@ using UnityEngine;
 public class GameLogic : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static float quickfalltime = 0.05f;
-    public static float falltime = 0.5f;
-    public static float height = 12f;
-    public static float width = 6f;
     public GameObject[] blocks;
-    private static int heightOfInt = (int)height;
-    private static int widthOfInt = (int)width;
-    public Transform[,] grid = new Transform[widthOfInt,heightOfInt];
     void Start()
     {
         SpawnBlock();
--- Tetris/Assets/TetrisSport.cs
+++ Tetris/Assets/TetrisSport.cs
@@ -8,11 +8,14 @@ public class TetrisSport : MonoBehaviour
     bool movable = true;
     public static float time = 0;
     public GameObject rig;
-    GameLogic gameLogic; 
-    public Vector3 rotationPoint;
+    public Transform[,] grid = new Transform[width,height];//儲存有方塊的位置
+    private float quickfalltime = 0.05f;
+    public  float falltime = 0.5f;
+    public static int height = 23;//因為掉落的初始位置(Spawn)在(5,21)，避免grid陣列超出範圍
+    public static int width = 10;
     void Start()
     {
-        gameLogic = FindObjectOfType<GameLogic>();
+        
     }
 
     // Update is called once per frame
@@ -21,50 +24,98 @@ public class TetrisSport : MonoBehaviour
         if(movable)
         {
             time += 1 * Time.deltaTime;
-            if(time > GameLogic.quickfalltime && Input.GetKey(KeyCode.DownArrow))
+            if(time > quickfalltime && Input.GetKey(KeyCode.DownArrow))
             {
-                gameObject.transform.position += new Vector3 (0,-0.6f,0);
+                transform.position += new Vector3 (0,-1,0);
                 time = 0;
                 if(!ValidMove()) 
                 {
                     movable = false;
-                    gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+                    transform.position -= new Vector3 (0,-1,0);
                     RegisterBlock();
-                    gameLogic.SpawnBlock();
+                    CheckLine();
+                    FindObjectOfType<GameLogic>().SpawnBlock();
                 }    
                 //Debug.Log(gameObject.transform.position);
             }
-            else if(time > GameLogic.falltime)
+            else if(time > falltime)
             {
-                gameObject.transform.position += new Vector3 (0,-0.6f,0);
+                transform.position += new Vector3 (0,-1,0);
                 time = 0;
-                if(!ValidMove()) 
+                if(!ValidMove())
                 {
                     movable = false;
-                    gameObject.transform.position -= new Vector3 (0,-0.6f,0);
+                    transform.position -= new Vector3 (0,-1,0);
                     RegisterBlock();
-                    gameLogic.SpawnBlock();
+                    CheckLine();
+                    FindObjectOfType<GameLogic>().SpawnBlock();
                 }    
                 //Debug.Log(gameObject.transform.position);
             }
             if(Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                gameObject.transform.position += new Vector3 (-0.6f,0,0);
+                transform.position += new Vector3 (-1,0,0);
                 if(!ValidMove()) 
-                    gameObject.transform.position -= new Vector3 (-0.6f,0,0);
+                    transform.position -= new Vector3 (-1,0,0);
             }
             else if(Input.GetKeyDown(KeyCode.RightArrow))
             {
-                gameObject.transform.position += new Vector3 (0.6f,0,0);
+                transform.position += new Vector3 (1,0,0);
                 if(!ValidMove())
-                    gameObject.transform.position -= new Vector3 (0.6f,0,0);
+                    transform.position -= new Vector3 (1,0,0);
             }
             else if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 //rotate !
-                rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
+                transform.Rotate(0,0,-90);//rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
                 if(!ValidMove())
-                    rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+                    transform.Rotate(0,0,90);//rig.transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+            }
+        }
+    }
+    void CheckLine()
+    {
+        for(int i = height-1 ; i >= 0 ; i--)
+        {
+            if(HasLine(i))
+            {
+                DeleteLine(i);
+                RowDown(i);
+            }
+        }
+    }
+    bool HasLine(int i)
+    {
+        for(int j = 0 ; j < width ; j++)
+        {
+            if(grid[j,i] == null)
+            {
+                return false;
+            }
+        }
+        Debug.Log("HasLine");
+        return true;
+    }
+    void DeleteLine(int i)
+    {
+        for(int j = 0 ; j < width ; j++)
+        {
+            Destroy(grid[j,i].gameObject);
+            grid[j,i] = null;
+        }
+    }
+    void RowDown(int i)
+    {
+        for(int y = i ; y < height ; y++)
+        {
+            for(int j = 0 ; j < width ; j++)
+            {
+                if(grid[j,y] != null)
+                {
+                    grid[j,y-1] = grid[j,y];
+                    grid[j,y] = null;
+                    grid[j,y-1].transform.position -= new Vector3(0,1,0);
+                }
             }
         }
     }
@@ -72,26 +123,33 @@ public class TetrisSport : MonoBehaviour
     {
         foreach(Transform subBlock in rig.transform)
         {
-            gameLogic.grid[Mathf.RoundToInt(subBlock.position.x), Mathf.RoundToInt(subBlock.position.y)] = subBlock;
+            int roundX = Mathf.RoundToInt(subBlock.transform.position.x);
+            int roundY = Mathf.RoundToInt(subBlock.transform.position.y);
+            grid[roundX , roundY] = subBlock;//在每個方塊的(x,y)儲存
+            /*Debug.Log(roundX);
+            Debug.Log(roundY);*/
         }
     }
     bool ValidMove()
     {
         foreach(Transform subBlock in rig.transform)
         {
-            if( subBlock.transform.position.x < 0 ||
-                subBlock.transform.position.x >= GameLogic.width ||
-                subBlock.transform.position.y < 0 )
+            int roundX = Mathf.RoundToInt(subBlock.transform.position.x);
+            int roundY = Mathf.RoundToInt(subBlock.transform.position.y);
+
+            if( roundX < 0 || roundX >= width || roundY < 0 )
             {
-                print(subBlock.name);
-                Debug.Log(subBlock.transform.position.x);
-                Debug.Log(subBlock.transform.position.y);
+                /*print(subBlock.name);
+                Debug.Log(roundX);
+                Debug.Log(roundY);*/
                 return false;
             }
-            if(gameLogic.grid[Mathf.RoundToInt(subBlock.position.x), Mathf.RoundToInt(subBlock.position.y)] != null)
+
+            if(grid[roundX, roundY] != null)
             {
+                Debug.Log("False!");
                 return false;
-            }
+            } 
         }
         return true;
     }
